b56af507da43112bd88ad92615559ac873c34e35
Hércules S. S. José <hercules_hssj@outlook.com>
2020-02-01 20:07:14 -0300

    Saída formatada.

--- udemy-curso-python-projetos-reais/aulaVariaveis.py
+++ udemy-curso-python-projetos-reais/aulaVariaveis.py
@@ -11,3 +11,11 @@ if __name__ == '__main__':
     imc = peso / altura ** 2
 
     print(nome, 'tem', idade, 'anos e seu IMC é', imc)
+
+    # Formatando a saída com F-String
+    print("Usando F-String...")
+    print(f'{nome} tem {idade} anos e seu IMC é {imc:.2f}')
+    print("Usando a função 'format()'")
+    print('{0} tem {1} anos e seu IMC é {2}'.format(nome, idade, imc))  # Pode usar índice nas chaves
+    print('{a} tem {b} anos e seu IMC é {c}'.format(a=nome, b=idade, c=imc))  # Pode usar parâmetros nomeados
+
