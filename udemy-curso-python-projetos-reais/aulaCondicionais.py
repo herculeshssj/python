d9eb8f48afac673541def2116a0f902dedd4f86f
Hércules S. S. José <hercules_hssj@outlook.com>
2020-02-05 20:40:36 -0300

    Melhorando o programa usando funções para validar os números digitados.

--- udemy-curso-python-projetos-reais/aulaCondicionais.py
+++ udemy-curso-python-projetos-reais/aulaCondicionais.py
@@ -1,3 +1,5 @@
+from libs.chk_numbers import is_number
+
 if __name__ == '__main__':
     print('Aula condicionais - IF, ELIF, ELSE')
 
@@ -38,28 +40,35 @@ if __name__ == '__main__':
         print('Por favor informe um valor para b')
 
     nome = input('Qual é o seu nome? ')
-    idade = int(input('Qual a sua idade? '))
+    idadeStr = input('Qual a sua idade? ')
+    idade = 0
 
-    print(f'Seu nome é {nome}.\n\r')
-    if idade >= 18:
-        print(f'{nome} é maior de idade.')
-    elif idade < 0:
-        print(f'Idade não determinada!')
+    # Verifica se a idade é numérica
+    if is_number(idadeStr):
+        idade = int(idadeStr)
+
+        print(f'Seu nome é {nome}.\n\r')
+        if idade >= 18:
+            print(f'{nome} é maior de idade.')
+        elif idade < 0:
+            print(f'Idade não determinada!')
+        else:
+            print(f'{nome} é menor de idade.')
+
+        idade_minima = 20
+        idade_maxima = 30
+
+        if idade >= idade_minima and idade <= idade_maxima:
+            print(f'{nome} pode pegar o empréstimo.')
+        else:
+            print(f'{nome} não pode pegar o empréstimo.')
+
+        if 'a' in nome or 'e' in nome or 'i' in nome or 'o' in nome or 'u' in nome:
+            print(f'{nome}, seu nome possui vogais.')
+        else:
+            print(f'{nome}, seu nome não possui vogais.')
     else:
-        print(f'{nome} é menor de idade.')
-
-    idade_minima = 20
-    idade_maxima = 30
-
-    if idade >= idade_minima and idade <= idade_maxima:
-        print(f'{nome} pode pegar o empréstimo.')
-    else:
-        print(f'{nome} não pode pegar o empréstimo.')
-
-    if 'a' in nome or 'e' in nome or 'i' in nome or 'o' in nome or 'u' in nome:
-        print(f'{nome}, seu nome possui vogais.')
-    else:
-        print(f'{nome}, seu nome não possui vogais.')
+        print('Idade informada não é um número válido!')
 
     # Aproveitando a aula da função len()
     print(f'Total de caracteres: {len(nome)}')
---
+++ udemy-curso-python-projetos-reais/calculadora.py
@@ -0,0 +1,9 @@
+'''
+Programa Calculadora
+'''
+
+if __name__ == '__main__':
+    print('Programa Calculadora')
+    print()
+    print('Instruções: informe o primeiro valor, o segundo valor e a operação desejada (+, -, *, /)')
+    print()
