282a33d63ad4c3f491d70e9d9b777c0ee793b88d
Hércules S. S. José <hercules_hssj@outlook.com>
2020-02-24 14:28:48 -0300

    Preparando para resolver o desafio 10.

---
+++ udemy-curso-python-projetos-reais/desafio10.py
@@ -0,0 +1,17 @@
+"""
+Desafio 10
+
+A partir desta string '01234567890123456789012345678901234567890123456789'
+Gerar a seguinte lista:
+lista = ['0123456789', '0123456789', '0123456789', '0123456789', '0123456789']
+
+E após isso gerar a segunte string
+
+'0123456789.0123456789.0123456789.0123456789.0123456789'
+
+Deve-se usar somente list comprehension
+
+"""
+
+if __name__ == '__main__':
+    print('Desafio 10 - List Comprehension')
