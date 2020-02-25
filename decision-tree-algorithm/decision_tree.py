0a2d58a5482ef8ac8f93a7933df3c72575e0a0d0
Hércules S. S. José <hercules_hssj@outlook.com>
2020-02-25 13:43:56 -0300

    Implementação completa do algoritmo de árvore de decisão.

--- decision-tree-algorithm/decision_tree.py
+++ decision-tree-algorithm/decision_tree.py
@@ -1,3 +1,66 @@
+# CART on the Bank Note dataset
+from random import seed
+from random import randrange
+from csv import reader
+
+
+# Load a CSV file
+def load_csv(filename):
+    file = open(filename, "rt")
+    lines = reader(file)
+    dataset = list(lines)
+    return dataset
+
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+# Evaluate an angorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = evaluate_algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+
+    return scores
+
 # Calculate the Gini index for a split dataset
 def gini_index(groups, classes):
 
@@ -114,6 +177,31 @@ def print_tree(node, depth=0):
         print('%s[%s]' % ((depth*' ', node)))
 
 
+# Make a prediction with a decision tree
+def predict(node, row):
+    if row[node['index']] < node['value']:
+        if isinstance(node['left'], dict):
+            return predict(node['left'], row)
+        else:
+            return node['left']
+    else:
+        if isinstance(node['right'], dict):
+            return predict(node['right'], row)
+        else:
+            return node['right']
+
+
+# Classification and Regression Tree Algorithm
+def decision_tree(train, test, max_depth, min_size):
+	tree = build_tree(train, max_depth, min_size)
+	predictions = list()
+	for row in test:
+		prediction = predict(tree, row)
+		predictions.append(prediction)
+	return(predictions)
+
+
+
 # First part - Gini index
 # test Gini values
 print(gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
@@ -131,11 +219,42 @@ dataset = [[2.771244718,1.784783929,0],
 	[7.444542326,0.476683375,1],
 	[10.12493903,3.234550982,1],
 	[6.642287351,3.319983761,1]]
-split = get_split(dataset)
-print('Split: [X%d < %.3f]' % ((split['index']+1), split['value']))
+split_result = get_split(dataset)
+print('Split: [X%d < %.3f]' % ((split_result['index']+1), split_result['value']))
 print('')
 
 # Third part - Build a tree
-tree = build_tree(dataset, 1, 1)
+tree = build_tree(dataset, 4, 1)
 print_tree(tree)
 print('')
+
+# Forth part - make a prediction
+#  predict with a stump
+stump = {'index': 0, 'right': 1, 'value': 6.642287351, 'left': 0}
+for row in dataset:
+	prediction = predict(stump, row)
+	print('Expected=%d, Got=%d' % (row[-1], prediction))
+
+# Banknote Study Case
+print('Bank Note Study Case')
+
+# Test CART on Bank Note dataset
+seed(1)
+
+# load and prepare data
+filename = 'data_banknote_authentication.csv'
+dataset = load_csv(filename)
+
+# convert string attributes to integers
+for i in range(len(dataset[0])):
+    str_column_to_float(dataset, i)
+
+# evaluate algorithm
+n_folds = 5
+max_depth = 5
+min_size = 10
+
+scores = evaluate_algorithm(dataset, decision_tree, n_folds, max_depth, min_size)
+
+print('Scores: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
