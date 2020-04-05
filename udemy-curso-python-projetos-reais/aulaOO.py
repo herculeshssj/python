c02b505b69e8859008ba2a8a1e153babb8f84b28
Hércules S. S. José <hercules_hssj@outlook.com>
2020-04-05 16:03:08 -0300

    Composição de objetos.

--- udemy-curso-python-projetos-reais/aulaOO.py
+++ udemy-curso-python-projetos-reais/aulaOO.py
@@ -1,3 +1,4 @@
+from oo.cliente import Cliente
 from oo.carrinho import CarrinhoDeCompras, ProdutoCarrinho
 from oo.escritor import MaquinaDeEscrever
 from oo.escritor import Caneta
@@ -108,3 +109,31 @@ carrinho.inserir_produto(p3)
 
 carrinho.lista_produto()
 print(carrinho.soma_total())
+
+"""
+Composição de objetos
+"""
+
+cliente1 = Cliente('Luiz', 32)
+cliente1.insere_endereco('Belo Horizonte', 'MG')
+print(cliente1.nome)
+cliente1.lista_enderecos()
+del cliente1
+print()
+
+cliente2 = Cliente('Maria', 55)
+cliente2.insere_endereco('Salvador', 'BA')
+cliente2.insere_endereco('Rio de Janeiro', 'RJ')
+print(cliente2.nome)
+cliente2.lista_enderecos()
+del cliente2
+print()
+
+cliente3 = Cliente('João', 19)
+cliente3.insere_endereco('São Paulo', 'SP')
+print(cliente3.nome)
+cliente3.lista_enderecos()
+del cliente3
+print()
+
+print('#########################################################')
