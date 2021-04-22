4f7084a903d9edbdb02ca9bd956093aca6863a03
Hércules S. S. José <hercules_hssj@outlook.com>
2021-04-22 09:29:23 -0300

    Comentários em Python.

---
+++ udemy-curso-python-projetos-reais/AulaComentarios/AulaComentario.py
@@ -0,0 +1,21 @@
+"""
+DocStrings
+
+Serve para comentários de múltiplas linhas.
+
+É utilizado aspas duplas.
+"""
+
+'''
+Posso usar DocStrings usando aspas simples
+'''
+# Este é o módulo principal
+if __name__ == '__main__':
+    # Isso é um comentário em Python
+    print('Comentários em Python')
+
+    # print('Linha comentada')
+    print('Comentário na mesma linha')  # Este comentário está na mesma linha
+
+    # Imprime o numeral 1 na tela
+    print(1)
