cb0013cf23857598f753b6a1b75713c360d1dd47
Hércules S. S. José <hercules_hssj@outlook.com>
2021-04-22 09:56:59 -0300

    String e uso de aspas.

---
+++ udemy-curso-python-projetos-reais/AulaStringAspas/AulaStringAspas.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+    print('Strings (texto) e aspas em Python')
+
+    """
+    str - string
+    """
+
+    print("Aspas duplas")
+    print('Aspas simples')
+
+    # Uso de aspas dentro da string
+    print('Essa é uma "string" (str)')
+    print("Essa é uma 'string' (str)")
+
+    # Uso de caracteres de escape
+    print('Esse é meu \'texto\' (str)')
+
+    # Quebra de linha
+    print('Esse é meu \n texto ')
+
+    # Informa para o interpretador que o conteúdo da string não deve ser ignorado
+    print(r'Esse é meu texto com \n"aspas')
