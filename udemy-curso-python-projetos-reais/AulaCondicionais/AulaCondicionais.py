2c4f1ef2c4f300400c2c645752381a1c10326865
Hércules S. S. José <hercules_hssj@outlook.com>
2021-04-26 09:41:24 -0300

    Condicionais - IF, ELIF, ELSE.

---
+++ udemy-curso-python-projetos-reais/AulaCondicionais/AulaCondicionais.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    print('Condicionais - IF, ELIF, ELSE, operadores relacionais e operadores lógicos')
+
+    if False:
+        print('Verdadeiro')
+    elif True:
+        print('Agora é verdadeiro')
+    elif False:
+        print('Mais uma verdadeira')
+    else:
+        print('Falso')
+
