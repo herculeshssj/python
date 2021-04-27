34fb5222c53afdb55ed09d6068635d0575116b3c
Hércules S. S. José <hercules_hssj@outlook.com>
2021-04-27 09:29:18 -0300

    Condicionais - IF, ELIF, ELSE - Operadores relacionais.

--- udemy-curso-python-projetos-reais/AulaCondicionais/AulaCondicionais.py
+++ udemy-curso-python-projetos-reais/AulaCondicionais/AulaCondicionais.py
@@ -10,3 +10,40 @@ if __name__ == '__main__':
     else:
         print('Falso')
 
+    """
+    Operadores relacionais
+    == > >= < <= !=
+    """
+    # Igualdade
+    print(f'Igualdade: 2 == 2 -> {2 == 2}')
+
+    # Maior que
+    print(f'Maior que: 2 > 2 -> {2 > 2}')
+
+    # Maior ou igual que
+    print(f'Maior ou igual que: 2 >= 2 -> {2 >= 2}')
+
+    # Menor que
+    print(f'Menor que: 2 < 2 -> {2 < 2}')
+
+    # Menor ou igual que
+    print(f'Menor ou igual que: 2 <= 2 -> {2 <= 2}')
+
+    # Diferenća
+    print(f'Diferenća: 2 != 2 -> {2 != 2}')
+
+    print()
+    print('***** Exercício *****')
+    nome = input('Qual é seu nome? ')
+    idade = input('Qual a sua idade? ')
+
+    # Cast
+    idade = int(idade)
+
+    # Limite para pegar empréstimo
+    idade_limite = 18
+
+    if idade >= idade_limite:
+        print(f'{nome} pode pegar o empréstimo.')
+    else:
+        print(f'{nome} não pode pegar o empréstimo.')
