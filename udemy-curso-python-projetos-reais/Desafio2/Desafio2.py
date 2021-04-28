ec3157c9d749712554546b13c5b66b838bb250dd
Hércules S. S. José <hercules_hssj@outlook.com>
2021-04-28 09:54:32 -0300

    Desafio 2 - início do exerćicio.

---
+++ udemy-curso-python-projetos-reais/Desafio2/Desafio2.py
@@ -0,0 +1,28 @@
+"""
+Faća um programa que peća ao usuário para digital um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+"""
+Faća um programa que pergunte a hora ao usuário e, baseando-se no horário
+descrito, exiba a saudaćão apropriada. Ex.
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+"""
+Faća um programa que peća o primeiro nome do usuário. Se o nome tiver 4 letras ou
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+
+if __name__ == '__main__':
+    print('##### Desafio 2 #####')
+
+    print('***** Exercício 1 *****')
+
+
+    print('***** Exercício 2 *****')
+
+
+    print('***** Exercício 3 *****')
