11bbb9928719eb979897cfd642009c7855c3a1f9
Hércules S. S. José <hercules_hssj@outlook.com>
2021-04-29 09:16:11 -0300

    Desafio 2 - resolućão dos exercícios 1 e 2.

--- udemy-curso-python-projetos-reais/Desafio2/Desafio2.py
+++ udemy-curso-python-projetos-reais/Desafio2/Desafio2.py
@@ -16,12 +16,37 @@ menos escreva "Seu nome é curto"; se tiver entre 5 e 6, escreva
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
 
+from AulaFuncoesBuiltIn.AulaFuncoesBuiltIn import *
+
 if __name__ == '__main__':
     print('##### Desafio 2 #####')
 
     print('***** Exercício 1 *****')
+    numero = input('Digite um número inteiro: ')
+    if is_int(numero):
+        numero = int(numero)
+        if numero % 2 == 0:
+            print('É par!')
+        else:
+            print('É ímpar!')
+    else:
+        print('Não é um número inteiro!')
 
     print('***** Exercício 2 *****')
+    hora_atual = input('Informe a hora atual (0-23): ')
+    if is_int(hora_atual):
+        hora_atual = int(hora_atual)
+        if hora_atual >=0 and hora_atual <= 23:
+            if hora_atual >=0 and hora_atual <=11:
+                print('Bom Dia!')
+            elif hora_atual > 11 and hora_atual <=17:
+                print('Boa Tarde!')
+            else:
+                print('Boa Noite!')
+        else:
+            print('Hora fora do intervalo!')
+    else:
+        print('Hora inválida!')
 
     print('***** Exercício 3 *****')
     nome_usuario = input('Digite seu nome: ')
