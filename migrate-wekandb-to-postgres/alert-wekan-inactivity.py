1592c9c4d23577e589e96f1cd17a8f6a522fc448
Hércules S. S. José <hercules_hssj@outlook.com>
2022-05-15 10:07:41 -0300

    Finalizado a implementação da consulta para o alerta.

--- migrate-wekandb-to-postgres/alert-wekan-inactivity.py
+++ migrate-wekandb-to-postgres/alert-wekan-inactivity.py
@@ -15,7 +15,39 @@ if __name__ == '__main__':
     # Seta as variáveis
     to_date = datetime.today()
     from_date = datetime.today() - timedelta(hours=72)
+
+    # Consulta as raias em movimento
+    raias_em_movimento = db.activities.aggregate([
+            {
+                "$match": {
+                    "userId": { 
+                        "$in": ["ng6ezgdC555We6fvr", "pk6eq8m7oqndKWdpe"]
+                    },
+                    "createdAt" : {
+                        "$gte": from_date,
+                        "$lt": to_date
+                    }
+                }
+            },
+            {
+                "$project" : {
+                    "swimlaneId": 1
+                }
+            },
+            {
+                "$group": {
+                    "_id": "_id",
+                    "activityId": {
+                        "$addToSet": "$swimlaneId"
+                    }
+                }
+            }
+        ])
     
+    raias_selecionadas = []
+    for r in raias_em_movimento:
+        raias_selecionadas = r['activityId']
+
     # Realiza a consulta
     inercia = db.activities.aggregate([
             {
@@ -39,13 +71,13 @@ if __name__ == '__main__':
                     "userId": {
                         "$in": ["ng6ezgdC555We6fvr", "pk6eq8m7oqndKWdpe"]
                     },
-                    "createdAt" : {
-                        "$lt": from_date
-                    },
                     "swimlane_table.archived": False,
                     "board_table.archived": False,
                     "board_table.title": {
                         "$ne": "Templates"
+                    },
+                    "swimlaneId" :{
+                        "$nin": raias_selecionadas
                     } 
                 }
             },
@@ -73,8 +105,6 @@ if __name__ == '__main__':
     mensagem_discord = 'Nenhuma atividade foi detectadas nos seguintes quadros:\n'
     for atividade in inercia:
         mensagem_discord = mensagem_discord + '* ' + atividade['boardName'][0] + ' -> ' + atividade['_id'][0] + '\n'
-
-    print(mensagem_discord)
 
     # Envia a mensagem de aviso
     webhook = DiscordWebhook(url=discord_url, content=mensagem_discord)
