94216de34ea3da833c5572ff914f72ba1697b321
Hércules S. S. José <hercules_hssj@outlook.com>
2022-06-04 11:44:51 -0300

    Removido código do projeto FiladoPao. O projeto foi arquivado.

--- alert-wekan/alert-wekan-inactivity.py
+++ alert-wekan/alert-wekan-inactivity.py
@@ -10,29 +10,6 @@ Posteriormente verei os alertas para as pessoas que
 estou devendo uma resposta.
 """
 
-"""
-def inicializa_base_sqlite():
-    # Conecta na base SQlite
-    conn = sqlite3.connect('filaespera.db')
-
-    # Objeto cursor
-    cursor = conn.cursor()
-
-    # Cria a tabela caso não exista
-    query = ""
-        create table if not exists fila_espera (
-            id text primary key not null,
-            cliente text not null,
-            pedido text not null
-        )
-    ""
-    cursor.execute(query)
-
-    # Commita as alterações e encerra a conexão
-    conn.commit()
-    conn.close()
-"""
-
 if __name__ == '__main__':
     # Conecta no MongoDB
     client = MongoClient('mongodb://root:root@localhost:27017')
@@ -179,77 +156,3 @@ if __name__ == '__main__':
         print(mensagem_discord) # Impressão no console, para fins de debug.
         webhook = DiscordWebhook(url=discord_url, content=mensagem_discord)
         response = webhook.execute()
-
-    """
-    Fila de espera para atendimento
-    ""
-
-    # Inicializa a base SQlite
-    inicializa_base_sqlite()
-
-    # Declaração dos objetos da base
-    conn = sqlite3.connect('filaespera.db')
-    cursor = conn.cursor()
-
-    limite_espera = datetime.today() - timedelta(hours=24)
-
-    # Busca os solicitantes que estão na fila de espera
-    fila_espera = db.cards.find(
-            {
-                "archived" : False,
-                "requestedBy": {
-                    "$not" : {
-                        "$eq" : ""
-                    }
-                },
-                "dateLastActivity": {
-                    "$lt": limite_espera
-                }
-            },
-            {
-                "_id": 1,
-                "title": 1,
-                "requestedBy" : 1
-            }
-        )
-
-    # Integração com o app Outsystems 'Fila do Pão' que informa as pessoas que preciso responder.
-    url_api = 'https://personal-8gsrdrii.outsystemscloud.com/filadopaoapi/rest/v1/novo'
-
-    # Itera a fila de espera para registrar no aplicativo
-    for cliente in fila_espera:
-        # Verifica se o cliente já entrou na fila de espera
-        cursor = conn.execute("select * from fila_espera where id = '" + cliente['_id'] + "'")
-        if not cursor.fetchall():
-            # Cadastra o cliente na base
-            queryInsert = ("insert into fila_espera (id, cliente, pedido) values (:ID, :CLIENTE, :PEDIDO);")
-            paramsInsert = {
-                'ID': cliente['_id'],
-                'CLIENTE': cliente['requestedBy'],
-                'PEDIDO': cliente['title']
-            }
-            conn.execute(queryInsert, paramsInsert)
-            conn.commit()
-
-            # Prepara a requisição
-            data = {
-                "Cliente": cliente['requestedBy'],
-                "Pedido": cliente['title'],
-                "PrazoAtendimentoId": 14
-            }
-            data_json = json.dumps(data)
-            headers = {'Content-type': 'application/json'}
-
-            # Registra o cliente na aplicação
-            response = requests.post(url=url_api, data=data_json, headers=headers)
-
-            print(response.json()) # impressão no console, para fins de debug'
-            print('Cliente ' ,cliente['requestedBy'], ' incluído na fila de espera.')
-        else:
-            print('Cliente ' ,cliente['requestedBy'], ' está na fila de espera.')
-
-
-    # Encerra a conexão com a base SQlite
-    conn.close()
-
-    """
