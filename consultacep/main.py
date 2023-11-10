bb7cf9abf17a8de27baa2bc0dcdd68441599abb3
Hércules S. S. José <hercules_hssj@outlook.com>
2023-11-10 16:27:28 -0300

    Implementado lógica para buscar e salvar o CEP.

--- consultacep/main.py
+++ consultacep/main.py
@@ -1,20 +1,48 @@
 from csv import reader
 import time
 import brazilcep
+import sqlite3
 
 if __name__ == '__main__':
-    print('Hello :)')
+    
+    # Conectando na base SQLite...
+    connection = sqlite3.connect("ceps.db")
+    cursor = connection.cursor()
 
-    print('Lendo o arquivo CSV...')
-
-    ### https://thispointer.com/write-to-a-csv-file-in-loop-in-python/
+    ### Descomente caso seja a primeira vez que o script está sendo executado
+    # cursor.execute("create table logradouro (district TEXT, cep TEXT, city TEXT, street TEXT, uf TEXT, complement TEXT)")
+    # connection.commit()
 
     with open('CEPs.csv', 'r') as read_obj:
         csv_reader = reader(read_obj)
         header = next(csv_reader)
         if header != None:
-            for row in csv_reader:
-                print(row[0])
-                address = brazilcep.get_address_from_cep(row[0])
-                print(address)
-                time.sleep(5)
+            for csv_row in csv_reader:
+                cep = "{}-{}".format(csv_row[0][:5], csv_row[0][5:])
+                print(cep)
+                rows = cursor.execute("select cep from logradouro where cep = ?", (cep,)).fetchall()
+                if len(rows) == 0:
+                    
+                    address = brazilcep.get_address_from_cep(csv_row[0])
+                    print(address)
+                    sql_tuple = (
+                        address['district'],
+                        address['cep'],
+                        address['city'],
+                        address['street'],
+                        address['uf'],
+                        address['complement']
+                    )
+
+                    cursor.execute("insert into logradouro (district, cep, city, street, uf, complement) values (?,?,?,?,?,?)", sql_tuple)
+                    connection.commit()
+
+                else:
+                    print('CEP ',csv_row[0], ' já existe, continuando...')
+                    continue
+
+                time.sleep(10)
+                
+    # Fechando os recursos
+    cursor.close()
+    connection.close()
