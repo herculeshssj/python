99caad87ea15b6b38a21350b6f351d501fca7832
Hércules S. S. José <hercules_hssj@outlook.com>
2023-11-11 07:48:49 -0300

    Ajustes finais no código

--- consultacep/main.py
+++ consultacep/main.py
@@ -23,25 +23,35 @@ if __name__ == '__main__':
                 rows = cursor.execute("select cep from logradouro where cep = ?", (cep,)).fetchall()
                 if len(rows) == 0:
                     
-                    address = brazilcep.get_address_from_cep(csv_row[0])
-                    print(address)
-                    sql_tuple = (
-                        address['district'],
-                        address['cep'],
-                        address['city'],
-                        address['street'],
-                        address['uf'],
-                        address['complement']
-                    )
+                    address = None
+                    try:
+                        address = brazilcep.get_address_from_cep(csv_row[0])
 
-                    cursor.execute("insert into logradouro (district, cep, city, street, uf, complement) values (?,?,?,?,?,?)", sql_tuple)
-                    connection.commit()
+                        print(address)
+                        sql_tuple = (
+                            address['district'],
+                            address['cep'],
+                            address['city'],
+                            address['street'],
+                            address['uf'],
+                            address['complement']
+                        )
+
+                        cursor.execute("insert into logradouro (district, cep, city, street, uf, complement) values (?,?,?,?,?,?)", sql_tuple)
+                        connection.commit()
+                    except brazilcep.exceptions.CEPNotFound as e:
+                        # Registra o CEP na base para que o mesmo não seja consultado novamente
+                        sql_tuple = ('-', cep, '-', '-', '-', '-')
+                        cursor.execute("insert into logradouro (district, cep, city, street, uf, complement) values (?,?,?,?,?,?)", sql_tuple)
+                        connection.commit()
+
+                        print('CEP ', csv_row[0], ' não encontrado, continuando...')
 
                 else:
                     print('CEP ',csv_row[0], ' já existe, continuando...')
                     continue
 
-                time.sleep(10)
+                time.sleep(15)
                 
     # Fechando os recursos
     cursor.close()
