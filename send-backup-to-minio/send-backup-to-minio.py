6e15314e34cd60ac2d9ed04298cb247cafb33337
Hércules S. S. José <hercules_hssj@outlook.com>
2024-06-11 23:49:54 -0300

    Nona tentativa de exclusão de arquivo

--- send-backup-to-minio/send-backup-to-minio.py
+++ send-backup-to-minio/send-backup-to-minio.py
@@ -25,7 +25,7 @@ def enviar_arquivo_para_minio(arquivo):
     print(f'Prefixo sem /data: {arquivo_prefixo.lstrip("/data/")}')
 
     # Liste todos os objetos no bucket
-    objects = client.list_objects(bucket_name, prefix="bash_history", recursive=True)
+    objects = client.list_objects(bucket_name, prefix="/data/bash_history", recursive=True)
     print('Lista de objetos')
     for o in objects:
         print(o)
