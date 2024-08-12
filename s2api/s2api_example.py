1ab9e1bd7cf23fb13bbc7f77924c902b947b9f06
Hércules S. S. José <hercules_hssj@outlook.com>
2024-08-12 10:38:56 -0300

    Exemplo funcional de busca.

--- s2api/s2api_example.py
+++ s2api/s2api_example.py
@@ -1,13 +1,17 @@
 import requests
+import json
 
 # Define the API endpoint URL
 url = 'https://api.semanticscholar.org/graph/v1/paper/search'
 
 # More specific query parameter
-query_params = {'query': 'quantum computing'}
+query_params = {
+   'query': 'web of things',
+   'fields': 'title,year,abstract,authors.name'
+}
 
 # Directly define the API key (Reminder: Securely handle API keys in production environments)
-api_key = 'your api key goes here'  # Replace with the actual API key
+api_key = 'api_key'  # Replace with the actual API key
 
 # Define headers with API key
 headers = {'x-api-key': api_key}
@@ -19,6 +23,8 @@ response = requests.get(url, params=query_params, headers=headers)
 if response.status_code == 200:
    response_data = response.json()
    # Process and print the response data as needed
-   print(response_data)
+   pretty_json = json.dumps(response_data, indent=4)
+   print(pretty_json)
+   #print(response_data)
 else:
    print(f"Request failed with status code {response.status_code}: {response.text}")
