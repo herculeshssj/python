e64240023fbf900e169cd78e7e43112fb60e7871
Hércules S. S. José <hercules_hssj@outlook.com>
2024-08-24 23:09:50 -0300

    Salvando os resultados do bulk search no MongoDB.

--- s2api/s2api_bulk_search.py
+++ s2api/s2api_bulk_search.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from pymongo import MongoClient
 
 # Define the API endpoint URL
 url = 'https://api.semanticscholar.org/graph/v1/paper/search/bulk'
@@ -7,8 +8,9 @@ url = 'https://api.semanticscholar.org/graph/v1/paper/search/bulk'
 # More specific query parameter
 # Queries
 # - ("web of things") | WoT
+# - web of things
 query_params = {
-   'query': 'web of things',
+   'query': '("web of things") | WoT',
    'fields': 'paperId,title,year,abstract,externalIds,url'
 }
 
@@ -26,7 +28,22 @@ if response.status_code == 200:
    response_data = response.json()
    # Process and print the response data as needed
    pretty_json = json.dumps(response_data, indent=4)
-   print(pretty_json)
+   #print(pretty_json)
    #print(response_data)
+
+   # Conexão com a base Mongo
+   client = MongoClient('mongodb://s2api:S2s3m4nt1cSch*l4r@localhost:27017')
+   db = client.s2api
+
+   for s2result in response_data['data']:
+      s2pretty = json.dumps(s2result, indent=4)
+      print(s2pretty)
+      print('**************************************************')
+
+      db.bulk_search.insert_one(s2result)
+
+   # Encerra a conexão com a base Mongo
+   client.close()
+
 else:
    print(f"Request failed with status code {response.status_code}: {response.text}")
