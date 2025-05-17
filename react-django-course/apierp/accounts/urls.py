1646278dd59c922d89fcc3b2b9eb2864973c0405
Hércules S. S. José <hercules_hssj@outlook.com>
2025-05-17 10:01:36 -0300

    Criação das primeiras views

---
+++ react-django-course/apierp/accounts/urls.py
@@ -0,0 +1,6 @@
+from accounts.views.signin import SignIn
+from django.urls import path
+
+urlpatterns = [
+    path('signin', SignIn.as_view()),
+]
---
+++ react-django-course/apierp/accounts/views/signin.py
@@ -0,0 +1,27 @@
+from views import BaseView
+from accounts.auth import Authentication
+from accounts.serializers import UserSerializer
+from rest_framework import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class SignIn(BaseView):
+    def post(self, request) -> None:
+        # Implement the sign-in logic here
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = Authentication.authenticate(self, email=email, password=password)
+        
+        token = RefreshToken.for_user(user)
+
+        enterprise = self.get_enterprise_user(user.id)
+
+        serializer = UserSerializer(user)
+
+        return Response({
+            "user": serializer.data,
+            "enterprise": enterprise,
+            "refresh": token.refresh,
+            "access_token" : token.access_token
+        })
+
