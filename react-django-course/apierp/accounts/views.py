ccb43a7186a895d4a18f5d5e86f8768241f7df7f
Hércules S. S. José <hercules_hssj@outlook.com>
2025-05-17 10:01:36 -0300

    Criação das primeiras views

--- react-django-course/apierp/accounts/views.py
+++ react-django-course/apierp/accounts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+# No tutorial, o instrutor excluiu este arquivo. Caso eu tenha problemas no projeto, 
+# posteriormente eu excluirei este arquivo.
