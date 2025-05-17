2e212bdb0a648bff6c14d242ce2af09a2e35a58e
Hércules S. S. José <hercules_hssj@outlook.com>
2025-05-17 14:29:39 -0300

    Recupera o projeto original, para agilizar o treinamento.

--- react-django-course/apierp/accounts/models.py
+++ react-django-course/apierp/accounts/models.py
@@ -1,30 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Permission
+from django.contrib.auth.models import AbstractBaseUser, Permission
 
 from companies.models import Enterprise
 
-# Create your models here.
-class User(AbstractUser):
-    name = models.CharField(max_length=150, blank=True)
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
     is_owner = models.BooleanField(default=True)
 
-    # USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
         return self.email
     
-
 class Group(models.Model):
-    name = models.CharField(max_length=150, blank=True)
+    name = models.CharField(max_length=85)
     enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
 
-
-class GroupPermission(models.Model):
+class Group_Permissions(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+    permission  = models.ForeignKey(Permission, on_delete=models.CASCADE)
 
-
-class UserGroup(models.Model):
+class User_Groups(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
