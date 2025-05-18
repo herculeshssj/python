34478902912322254fe7093bdcbd5a330e94668e
Hércules S. S. José <hercules_hssj@outlook.com>
2025-05-18 10:28:05 -0300

    Criando os primeiros componentes.

--- react-django-course/primeiro-projeto-cra/src/App.tsx
+++ react-django-course/primeiro-projeto-cra/src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { UserName } from "./UserName";
+import { UserName as Us, UserEmail as Eu } from "./UserName";
+import UserAge from "./UserName";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meu primeiro componente de usuário</h1>
+
+      <Us />
+      <Eu />
+      <UserAge />
     </div>
   );
 }
