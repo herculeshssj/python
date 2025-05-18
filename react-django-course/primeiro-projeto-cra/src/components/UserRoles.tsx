1565bae6deda6e1ad54be05a27acd13a343abf1a
Hércules S. S. José <hercules_hssj@outlook.com>
2025-05-18 14:28:07 -0300

    Usando filtros

--- react-django-course/primeiro-projeto-cra/src/components/UserRoles.tsx
+++ react-django-course/primeiro-projeto-cra/src/components/UserRoles.tsx
@@ -6,10 +6,16 @@ export const UserRoles = () => {
         { id: 2, name: 'CTO' },
         { id: 3, name: 'admin' }
     ]
+
+    const filterRoles = (value: {id: number, title: string}):boolean => {
+        return value.title.includes('a');
+    }
+
+    const filteredRoles = rolesMap.filter(value => value.id == 1);
     
     return (
         <ul>
-            {rolesMap.map((value, key) => (
+            {filteredRoles.map((value, key) => (
                 <li key={key}>{value.name}</li>
             ))}
         </ul>
