185fc7d075950bedb51b7ffa4cb4cbdd5c51686c
Hércules S. S. José <hercules_hssj@outlook.com>
2025-05-18 12:26:49 -0300

    Exibição condicional

--- react-django-course/primeiro-projeto-cra/src/App.tsx
+++ react-django-course/primeiro-projeto-cra/src/App.tsx
@@ -1,10 +1,36 @@
 import { UserInfo } from "./components/UserInfo";
 
 const App = () => {
+
+  const isLoggedIn = true;
+
+  /*
+  const isLoggedIn = false;
+
+  if (isLoggedIn) {
+
+    return (
+      <div className="App">
+        <h1>Meu primeiro componente de usuário</h1>
+        <UserInfo />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>Meu primeiro componente de usuário</h1>
+        <h2>Você não está logado</h2>
+      </div>
+    );
+  }
+    **/
+
   return (
     <div className="App">
       <h1>Meu primeiro componente de usuário</h1>
-      <UserInfo />
+
+      {isLoggedIn ? <UserInfo/> : <h2>Você não está logado</h2>}
+
     </div>
   );
 }
