be097bea7e9cf925c3157934ce5ae1b1eeb59461
Hércules S. S. José <hercules_hssj@outlook.com>
2025-05-18 12:18:33 -0300

    Variáveis e funções.

--- react-django-course/primeiro-projeto-cra/src/components/UserName.tsx
+++ react-django-course/primeiro-projeto-cra/src/components/UserName.tsx
@@ -1,5 +1,20 @@
 export const UserName = () => {
+
+  let name = "João da Silva";
+  let names = {nome1: 'João da Silva', nome2: 'Maria da Silva', nome3: 'José da Silva'};
+  
+  const formatName = (value: string) => {
+    return value.toUpperCase();
+  }
+
+  const nomeMaiusculo = (value: string) => value.toUpperCase();
+
   return (
-    <h4>Meu nome é: José</h4>
+    <>
+    <h4>Meu nome é: {name} </h4>
+    <h4>Meu nome é: {names.nome2} </h4>
+    <h4>Meu nome é: { formatName(name) } </h4>
+    <h4>Meu nome é: { nomeMaiusculo(name) } </h4>
+    </>
   );
 }
