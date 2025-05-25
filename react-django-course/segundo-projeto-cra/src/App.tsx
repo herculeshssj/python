9fb6d0e485252cec398b2a10ece7c137f5f20833
Hércules S. S. José <hercules@tic.ufrj.br>
2025-05-25 19:31:19 -0300

    Usando hooks

--- react-django-course/segundo-projeto-cra/src/App.tsx
+++ react-django-course/segundo-projeto-cra/src/App.tsx
@@ -1,7 +1,21 @@
 import { FormEvent } from "react";
 import { Button } from "./components/Button";
+import {useState} from 'react';
 
 const App = () => {
+
+  // Hooks são funções que permitem usar o estado e outros recursos do React sem escrever uma classe.
+  const [count, setCount] = useState(0)
+
+  const [logged, setLogged] = useState(false);
+
+  const handleClickCount = () => {
+    setCount(count + 1);
+  }
+
+  const handleLogin = () => {
+    setLogged(!logged);
+  }
 
   const handleClick = (value: string) => {
     alert(value);
@@ -21,6 +35,17 @@ const App = () => {
       <Button
         onClique={tratarClique}
       />
+
+      <h2>{count} cliques</h2>
+      <button onClick={handleClickCount}>
+        Usando Hooks - adicionar mais um
+      </button>
+
+      <h2>{logged ? 'Usuário logado' : 'Usuário não logado'}</h2>
+      <button onClick={handleLogin}>
+        {logged ? 'Deslogar' : 'Logar'}
+      </button>
+
       <form onSubmit={handleSubmit}>
         <input />
         <button>Enviar</button>
