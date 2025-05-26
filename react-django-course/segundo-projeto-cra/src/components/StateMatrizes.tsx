0078fbca198b6a8436704a4d7aae0df5e2d7c1ed
Hércules S. S. José <hercules@tic.ufrj.br>
2025-05-26 20:45:20 -0300

    Alterar states

--- react-django-course/segundo-projeto-cra/src/components/StateMatrizes.tsx
+++ react-django-course/segundo-projeto-cra/src/components/StateMatrizes.tsx
@@ -2,25 +2,26 @@ import { useState } from "react"
 
 export const StateMatrizes = () => {
 
-    const [tasks, setTasks] = useState<string[]>([])
+    //const [tasks, setTasks] = useState<string[]>([])
+    const [tasks, setTasks] = useState<{label: string, done: boolean}[]>([])
     const [inputValue, setInputValue] = useState('')
 
     const handleAdd = () => {
         if (inputValue == '') return;
 
         // Método 1
-        //setTasks([...tasks, inputValue])
-        //setInputValue('')
+        setTasks([...tasks, {label: inputValue, done: false}])
+        setInputValue('')
 
         // Método 2
         //setTasks(tasks.concat(inputValue))
         //setInputValue('')
 
         // Método 3
-        const newTasks = [...tasks]
-        newTasks.push(inputValue)
-        setTasks(newTasks)
-        setInputValue('')
+        //const newTasks = [...tasks]
+        //newTasks.push(inputValue)
+        //setTasks(newTasks)
+        //setInputValue('')
 
     }
 
@@ -28,6 +29,13 @@ export const StateMatrizes = () => {
         setTasks(tasks.filter((value, index) => index !== key))
     }
 
+    const handleEdit = (key: number, newValue: string) => {
+        const newTasks = [...tasks]
+        newTasks[key].done = !newTasks[key].done
+        setTasks(newTasks)
+
+    }
+        
     return(
         <div>
             <h2>Minha lista de tarefas</h2>
@@ -40,7 +48,12 @@ export const StateMatrizes = () => {
             <ul>
                 {tasks.map((task, index) => (
                     <li key={index}>
-                        {task}
+                        <input 
+                            type="checkbox" 
+                            checked={task.done}
+                            onChange={() => handleEdit(index, task.label)}
+                        />
+                        {task.label}
                         <button onClick={() => handleRemove(index)}>Remover</button>
                     </li>
                 ))}
