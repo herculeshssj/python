64b47ebed7a51c1a703932a8ea29622d792b05f2
Hércules S. S. José <hercules@tic.ufrj.br>
2025-05-27 09:53:39 -0300

    Uso prático de useEffect

--- react-django-course/segundo-projeto-cra/src/components/Card.tsx
+++ react-django-course/segundo-projeto-cra/src/components/Card.tsx
@@ -1,18 +1,45 @@
-import { use, useEffect } from "react"
+import { use, useEffect, useState } from "react"
 
 export const Card = () => {
-    useEffect(() => {
-        console.log('Card foi renderizado')
 
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [fullName, setFullName] = useState('');
+
+
+    useEffect(() => {
+        //console.log('Card foi renderizado')
+
+        /*
         return () => {
             console.log('Card foi desmontado')
         }
-    }, [])
+        */
+        setFullName(`${firstName} ${lastName}`);
+
+        return () => { 
+            console.log('useEffect foi executado!')
+        }
+    }, [firstName, lastName])
 
 
     return (
         <>
-            <p>Componente Card</p>
+            {/* <p>Componente Card</p> */}
+
+            <input
+                type="text"
+                placeholder="Nome"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Sobrenome"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+            />
+            <strong>{fullName}</strong>
         </>
     )
 }
