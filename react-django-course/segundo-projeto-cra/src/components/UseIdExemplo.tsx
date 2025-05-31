cc36702a598e236563de5c2aed7f32bb3a59201a
Hércules S. S. José <hercules@tic.ufrj.br>
2025-05-31 09:49:18 -0300

    Exemplo de useRef

--- react-django-course/segundo-projeto-cra/src/components/UseIdExemplo.tsx
+++ react-django-course/segundo-projeto-cra/src/components/UseIdExemplo.tsx
@@ -1,12 +1,24 @@
 import {useId} from 'react'
+import {useRef} from 'react'
 
 export const UseIdExemplo = () => {
     const id = useId();
+    const inputElemento = useRef<HTMLInputElement | null>(null);
+
+    const handleClick = () => {
+        if (inputElemento.current) {
+            inputElemento.current.focus();
+        }
+    }
 
     return (
         <>
             <label htmlFor={id}>Informe seu nome</label>
             <input placeholder="Digite seu nome" id={id} />
+
+            <br/>
+            <input id="input-1" placeholder="Input 1" ref={inputElemento} />
+            <button onClick={handleClick}>Focar no Input</button>
         </>
     )
 }
