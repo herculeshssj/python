647d582bd59b7cb868bcfecacf321e8ee3685c6a
Hércules S. S. José <hercules@tic.ufrj.br>
2025-05-31 12:47:05 -0300

    Outro uso de useRef

--- react-django-course/segundo-projeto-cra/src/components/UseIdExemplo.tsx
+++ react-django-course/segundo-projeto-cra/src/components/UseIdExemplo.tsx
@@ -1,9 +1,26 @@
-import {useId} from 'react'
+import {useId, useState} from 'react'
 import {useRef} from 'react'
 
 export const UseIdExemplo = () => {
     const id = useId();
     const inputElemento = useRef<HTMLInputElement | null>(null);
+
+    const [time, setTime] = useState(0);
+    const intervalRef = useRef(0);
+
+    const handleStart = () => {
+        if (intervalRef.current) handleStop();
+
+        /*const intervalID =*/ setInterval(() => {
+            setTime(t => t + 1)
+        }, 1000);
+
+        //intervalRef.current = intervalID;
+    }
+
+    const handleStop = () => {
+        clearInterval(intervalRef.current);
+    }
 
     const handleClick = () => {
         if (inputElemento.current) {
@@ -19,6 +36,13 @@ export const UseIdExemplo = () => {
             <br/>
             <input id="input-1" placeholder="Input 1" ref={inputElemento} />
             <button onClick={handleClick}>Focar no Input</button>
+
+            <br/>
+            <h2>Contador: {time}</h2>
+            <div style={{display: 'flex', gap: '10px'}}>
+                <button onClick={handleStart}>Iniciar</button>
+                <button onClick={handleStop}>Parar</button>
+            </div>
         </>
     )
 }
