52b98279ece23f4e8290c8d495a6ba677d605aa9
Hércules S. S. José <hercules@tic.ufrj.br>
2025-06-01 09:25:38 -0300

    Exibindo os dados da requisição GET

--- react-django-course/requisicoes-vite/src/App.tsx
+++ react-django-course/requisicoes-vite/src/App.tsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 const App = () => {
+
+  const [posts, setPosts] = useState<Post[]>([]);
 
   const handleGetPosts = async () => {
     const posts = await fetch('https://jsonplaceholder.typicode.com/posts',
@@ -10,12 +21,21 @@ const App = () => {
       })
       .then(response => response.json())
       .catch(error => console.error('Error fetching posts:', error));
-    console.log(posts);
+    
+    setPosts(posts);
   }
 
   return (
     <div>
       <button onClick={handleGetPosts}>Get Posts</button>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
