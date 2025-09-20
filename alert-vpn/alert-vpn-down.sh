9596a842c080953e54407ccba79b6b5b71f76f1c
Hércules S. S. José <hercules_hssj@outlook.com>
2025-09-20 15:06:40 -0300

    Alerta de VPN usando a API do Telegram.

--- alert-vpn/alert-vpn-down.sh
+++ alert-vpn/alert-vpn-down.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run --rm --net=host -e DISCORD_URL="<discord_url>" -e IP_CHECK="<IP to check>" -e PORT_CHECK="<port to check>" alert-vpn-down:latest
+docker run \
+    --rm \
+    --net=host \
+    -e TELEGRAM_TOKEN="<Token>" \
+    -e TELEGRAM_CHAT_ID="<chat_id>" \
+    -e IP_CHECK="<ip>" \
+    -e PORT_CHECK="<porta>" \
+    -e ERROR_MESSAGE="<mensagem>" \
+    alert-vpn-down:latest
