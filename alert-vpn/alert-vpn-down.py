cce672d4d69a97b3968420624a3f8c70ab6a86f9
Hércules S. S. José <hercules_hssj@outlook.com>
2025-09-20 15:06:40 -0300

    Alerta de VPN usando a API do Telegram.

--- alert-vpn/alert-vpn-down.py
+++ alert-vpn/alert-vpn-down.py
@@ -1,33 +1,36 @@
 import os
 import subprocess
-from discord_webhook import DiscordWebhook
+import requests
 
 """
-Alerta via Discord quando a conexão com o Raspberry PI caiu
+Alerta via Telegram quando a conexão com o Raspberry PI caiu
 
 Dica obtida de https://stackoverflow.com/questions/4760215/running-shell-command-and-capturing-the-output
 """
 
 if __name__ == '__main__':
     # Obtém as variáveis de ambiente
-    discord_url = os.getenv('DISCORD_URL')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
     ip_check = os.getenv('IP_CHECK')
     port_check = os.getenv('PORT_CHECK')
-
+    mensagem_telegram = os.getenv('ERROR_MESSAGE')
 
     # Executa o comando nc
     result = subprocess.run(['nc', '-z', '-v', ip_check, port_check], stdout=subprocess.PIPE)
     print(result)
 
-    if "returncode=0" in str(result):
+    if result.returncode == 0:
         print('VPN up! :)')
     else:
         print('VPN down :(')
 
-        # Constrói a mensagem
-        mensagem_discord = 'VPN caiu! Verifique a conexão com o Raspberry PI...'
-        
-        # Envia a mensagem de aviso
-        print(mensagem_discord) # Impressão no console, para fins de debug.
-        webhook = DiscordWebhook(url=discord_url, content=mensagem_discord)
-        response = webhook.execute()
+        # Envia a mensagem de aviso via Telegram
+        print(mensagem_telegram)  # Impressão no console, para fins de debug.
+        url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
+        payload = {
+            "chat_id": telegram_chat_id,
+            "text": mensagem_telegram
+        }
+        response = requests.post(url, data=payload)
+        print(response.text)
