482628c6985eb54648ee44aeb71015aab7f69ec8
hercules.jose <hercules.jose@presidencia.gov.br>
2026-01-29 15:52:46 -0300

    RIC models

--- migratedbs/models.py
+++ migratedbs/models.py
@@ -129,3 +129,239 @@ class Address(models.Model):
 
     def __str__(self):
         return f"{self.address_name}, {self.address_number} - {self.city or ''}"
+
+# ----------------------------
+# Tabelas do RIC
+# ----------------------------
+
+class TipoRequerimento(models.Model):
+    nom_tipo_requerimento = models.CharField(max_length=50)
+    sigla_tipo_requerimento = models.CharField(max_length=3)
+    nom_alias = models.CharField(max_length=50)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()  # use auto_now=True se quiser atualizar automaticamente
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_tipo_requerimento'
+        verbose_name = 'Tipo de Requerimento'
+        verbose_name_plural = 'Tipos de Requerimento'
+
+    def __str__(self):
+        return f'{self.sigla_tipo_requerimento} - {self.nom_tipo_requerimento}'
+
+
+class OrigemRequerimento(models.Model):
+    nom_orig_requerimento = models.CharField(db_column='nom_origem_requerimento', max_length=50)
+    nom_alias = models.CharField(max_length=50)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_origem_requerimento'
+        verbose_name = 'Origem do Requerimento'
+        verbose_name_plural = 'Origens do Requerimento'
+
+    def __str__(self):
+        return self.nom_orig_requerimento
+
+
+class StatusRequerimento(models.Model):
+    nom_status_requerimento = models.CharField(max_length=50)
+    nom_alias = models.CharField(max_length=50)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_status_requerimento'
+        verbose_name = 'Status do Requerimento'
+        verbose_name_plural = 'Status dos Requerimentos'
+
+    def __str__(self):
+        return self.nom_status_requerimento
+
+
+class TipoAutor(models.Model):
+    nom_tipo_autor = models.CharField(max_length=50)
+    nom_alias = models.CharField(max_length=50)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_tipo_autor'
+        verbose_name = 'Tipo de Autor'
+        verbose_name_plural = 'Tipos de Autor'
+
+    def __str__(self):
+        return self.nom_tipo_autor
+
+
+class TematicaRequerimento(models.Model):
+    nom_tematica_requerimento = models.CharField(max_length=50)
+    nom_alias = models.CharField(max_length=50)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_tematica_requerimento'
+        verbose_name = 'Temática do Requerimento'
+        verbose_name_plural = 'Temáticas do Requerimento'
+
+    def __str__(self):
+        return self.nom_tematica_requerimento
+
+
+class EtapaFluxo(models.Model):
+    nom_etapa_fluxo = models.CharField(max_length=30)
+    nom_alias = models.CharField(max_length=30)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_etapa_fluxo'
+        verbose_name = 'Etapa do Fluxo'
+        verbose_name_plural = 'Etapas do Fluxo'
+
+    def __str__(self):
+        return self.nom_etapa_fluxo
+
+
+class TipoResposta(models.Model):
+    nom_tipo_resposta = models.CharField(max_length=50)
+    nom_alias = models.CharField(max_length=50)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_tipo_resposta'
+        verbose_name = 'Tipo de Resposta'
+        verbose_name_plural = 'Tipos de Resposta'
+
+    def __str__(self):
+        return self.nom_tipo_resposta
+
+
+# ----------------------------
+# Autor e relacionamentos
+# ----------------------------
+
+class Autor(models.Model):
+    tipo_autor = models.ForeignKey(
+        TipoAutor,
+        on_delete=models.PROTECT,
+        db_column='tipo_autor_id',
+        related_name='autores'
+    )
+    nom_autor = models.CharField(max_length=200)
+    nom_alias = models.CharField(max_length=200)
+    partido = models.CharField(max_length=200, null=True, blank=True)
+    uf = models.CharField(max_length=2, null=True, blank=True)
+    flg_desativado = models.BooleanField(default=False)
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_autor'
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return self.nom_autor
+
+
+# ----------------------------
+# Requerimento
+# ----------------------------
+
+class Requerimento(models.Model):
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    num_requerimento = models.IntegerField()
+    ano_requerimento = models.IntegerField()
+
+    tipo_requerimento = models.ForeignKey(
+        TipoRequerimento, on_delete=models.PROTECT, db_column='tipo_requerimento_id', related_name='requerimentos'
+    )
+    origem_requerimento = models.ForeignKey(
+        OrigemRequerimento, on_delete=models.PROTECT, db_column='origem_requerimento_id', related_name='requerimentos'
+    )
+    status_requerimento = models.ForeignKey(
+        StatusRequerimento, on_delete=models.PROTECT, db_column='status_requerimento_id', related_name='requerimentos'
+    )
+
+    num_oficio = models.TextField()
+    data_recebimento_pr = models.DateField()
+    prazo_final_resposta_cn = models.DateField()
+
+    processo_sei_envio_cc = models.CharField(max_length=35)
+    processo_sei_resposta_cc = models.CharField(max_length=35)
+
+    dsc_objeto_requerimento = models.TextField()
+
+    tematica = models.ForeignKey(
+        TematicaRequerimento, on_delete=models.PROTECT, db_column='tematica_id', related_name='requerimentos'
+    )
+
+    # Comentado como JSON no script: usar JSONField
+    unidades_consultadas = models.JSONField()
+
+    status_congresso = models.TextField()
+    ementa = models.TextField()
+    andamento_cc = models.TextField()
+
+    etapa_fluxo = models.ForeignKey(
+        EtapaFluxo, on_delete=models.PROTECT, db_column='etapa_fluxo_id', related_name='requerimentos',
+        null=True, blank=True
+    )
+
+    prazo_etapa = models.TextField(null=True, blank=True)
+    dsc_resumo = models.TextField(null=True, blank=True)
+    dsc_observacoes = models.TextField(null=True, blank=True)
+
+    data_resposta_demandante = models.DateField()
+    tipo_resposta = models.ForeignKey(
+        TipoResposta, on_delete=models.PROTECT, db_column='tipo_resposta_id', related_name='requerimentos'
+    )
+
+    link_congresso = models.TextField(null=True, blank=True)
+    link_inteiro_teor = models.TextField(null=True, blank=True)
+
+    # ManyToMany através da tabela de junção com campos extras
+    autores = models.ManyToManyField('Autor', through='RequerimentoAutor', related_name='requerimentos')
+
+    class Meta:
+        db_table = 'ric_requerimento'
+        verbose_name = 'Requerimento'
+        verbose_name_plural = 'Requerimentos'
+        indexes = [
+            models.Index(fields=['ano_requerimento', 'num_requerimento']),
+        ]
+
+    def __str__(self):
+        return f'{self.num_requerimento}/{self.ano_requerimento}'
+
+
+class RequerimentoAutor(models.Model):
+    requerimento = models.ForeignKey(
+        Requerimento, on_delete=models.CASCADE, db_column='requerimento_id', related_name='autores_vinculos'
+    )
+    autor = models.ForeignKey(
+        Autor, on_delete=models.CASCADE, db_column='autor_id', related_name='requerimentos_vinculos'
+    )
+    dat_atualizacao = models.DateTimeField()
+    flg_ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'ric_requerimento_autor'
+        verbose_name = 'Autor do Requerimento'
+        verbose_name_plural = 'Autores do Requerimento'
+
+    def __str__(self):
+        return f'{self.requerimento} - {self.autor}'
