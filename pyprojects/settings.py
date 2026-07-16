36d65727421f7396c1ab4206d05d59f574f3cbc0
hercules.jose <hercules.jose@presidencia.gov.br>
2026-07-16 15:33:02 -0300

    Arquivamentoe de todos os apps do projeto Django desnecessário, junto com os projetos Python antigos.

--- pyprojects/settings.py
+++ pyprojects/settings.py
@@ -72,10 +72,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
-    'helloworld',
-    'migratedbs',
-    'exportwekan',
     'sendminio',
 ]
 
